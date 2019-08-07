ff79863ba4feea3a1e54f7fb3ca25af153371997
James Wilkinson <jdwilkinson1994@gmail.com>
2019-08-07 14:47:51 +0800

    cleaned up resize function, removed timeout and refactored with a promise

--- main.js
+++ main.js
@@ -5,42 +5,43 @@ const resizeImage = function(src, w, h) {
   const ctx = canvas.getContext("2d");
   canvas.width = w;
   canvas.height = h;
+  return new Promise(resolve => {
+    const img = new Image();
+    img.addEventListener("load", () => {
+      // https://stackoverflow.com/questions/19262141/resize-image-with-javascript-canvas-smoothly
+      canvas.height = canvas.width * (img.height / img.width);
 
-  const img = new Image();
+      // resize to 50%
+      const oc = document.createElement("canvas");
+      const octx = oc.getContext("2d");
 
-  img.onload = () => {
-    // https://stackoverflow.com/questions/19262141/resize-image-with-javascript-canvas-smoothly
-    canvas.height = canvas.width * (img.height / img.width);
+      oc.width = img.width * 0.5;
+      oc.height = img.height * 0.5;
+      octx.drawImage(img, 0, 0, oc.width, oc.height);
 
-    // resize to 50%
-    const oc = document.createElement("canvas");
-    const octx = oc.getContext("2d");
+      // step 2
+      octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
 
-    oc.width = img.width * 0.5;
-    oc.height = img.height * 0.5;
-    octx.drawImage(img, 0, 0, oc.width, oc.height);
-
-    // step 2
-    octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
-
-    // step 3 resize to final
-    ctx.drawImage(
-      oc,
-      0,
-      0,
-      oc.width * 0.5,
-      oc.height * 0.5,
-      0,
-      0,
-      canvas.width,
-      canvas.height
-    );
-  };
-  img.src = src;
-  return canvas;
+      // step 3 resize to final
+      ctx.drawImage(
+        oc,
+        0,
+        0,
+        oc.width * 0.5,
+        oc.height * 0.5,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+      );
+      resolve(canvas);
+    });
+    img.src = src;
+  });
 };
 
 const getClippedRegion = (img, x, y, width, height) => {
+  // Takes in an image and crops a specified chunk and returns a canvas element
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
 
@@ -52,33 +53,49 @@ const getClippedRegion = (img, x, y, width, height) => {
   return canvas;
 };
 
-const splitImage = (image, h, v, dest) => {
+const fragmentToCanvi = (image, w, h, dest) => {
+  // Splits an image into canvas elements of width w and height h and returns an array of canvas elements
   const ctx = dest.getContext("2d");
 
   const canvi = [];
-  for (let row = 0; row < v; row++) {
-    let y = row * 32;
-    for (let col = 0; col < h; col++) {
-      let x = col * 32;
-      const clip = getClippedRegion(image, x, y, 32, 32);
+  for (let row = 0; row < h; row++) {
+    let y = row * h;
+    for (let col = 0; col < w; col++) {
+      let x = col * w;
+      const clip = getClippedRegion(image, x, y, w, h);
       ctx.drawImage(clip, x, y);
       canvi.push(clip);
     }
   }
+  return canvi;
 };
-
-const canvas = document.querySelector("#canvas");
-const resizedImage = resizeImage("./images/a.jpg", 1024, 1024);
-
-// This does not need to be seen, but needs to be physically added to DOM to be recognised as an image correctly
-const temp = document.body.appendChild(resizedImage);
-temp.style.visibility = "hidden";
-temp.style.display = "none";
-setTimeout(() => {
-  splitImage(resizedImage, 32, 32, canvas);
-}, 1000);
 const toImages = canvi => {
   return canvi.map(canvas => canvas.toDataURL("image/jpg"));
 };
 
-// const images = toImages(canvi);
+const input = document.querySelector("#image");
+
+const canvas = document.querySelector("#canvas");
+const width = 1024;
+const height = 1024;
+
+const images = {
+  width: width,
+  height: height,
+  nCols: Math.sqrt(width),
+  nRows: Math.sqrt(height),
+  items: []
+};
+resizeImage(input.src, 1024, 1024).then(img => {
+  // This does not need to be seen, but needs to be physically added to DOM to be recognised as an image correctly
+  const temp = document.body.appendChild(img);
+  temp.style.visibility = "hidden";
+  temp.style.display = "none";
+
+  images.items = fragmentToCanvi(
+    img,
+    Math.sqrt(width),
+    Math.sqrt(height),
+    canvas
+  );
+});
