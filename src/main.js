c78f87cff79832ee42d6df4ef45e6dbc26f35125
James Wilkinson <jdwilkinson1994@gmail.com>
2019-08-09 12:27:35 +0800

    fixed padding and margin issues

--- src/main.js
+++ src/main.js
@@ -22,28 +22,53 @@ const main = async () => {
   grid.innerHTML = gridTemplate(mosaic);
   document.body.appendChild(grid);
 };
+//main();
 
-const App = {
-  events: {
-    submit: e => {
-      e.preventDefault();
-    }
-  },
-  $: {},
-  cacheDOM: function() {
-    return {
-      body: document.querySelector("body"),
-      submit: document.querySelector(".submit")
-    };
-  },
-  createEvents: events => {
-    document.addEventListener("click", events.submit);
-  },
-  deleteEvents: () => {},
-  render: function() {
-    this.deleteEvents();
-    this.createEvents(this.events);
-    Object.assign(this.$, this.cacheDOM());
-  }
+// const App = {
+//   events: {
+//     submit: e => {
+//       e.preventDefault();
+//     },
+//     clickHandlers: e => {
+//       if (e.target.classList.contains("submit")) {
+//         e.preventDefault();
+//       }
+//     }
+//   },
+//   $: {},
+//   cacheDOM: function() {
+//     this.$ = document.querySelector("body");
+//     return {
+//       submit: this.$.querySelector(".submit")
+//     };
+//   },
+//   createEvents: events => {
+//     const { clickHandlers } = events;
+//     document.addEventListener("click", clickHandlers);
+//   },
+//   deleteEvents: events => {
+//     const { clickHandlers } = events;
+//     document.removeEventListener("click", clickHandlers);
+//   },
+//   render: function() {
+//     this.deleteEvents(this.events);
+//     this.createEvents(this.events);
+//     Object.assign(this.$, this.cacheDOM());
+//   }
+// };
+// App.render();
+
+const $ = {
+  body: document.querySelector("body"),
+  submit: document.querySelector(".submit"),
+  form: document.querySelector("form")
 };
-App.render();
+
+const registerEvents = $ => {
+  $.submit.addEventListener("click", e => {
+    e.preventDefault();
+    console.log($.form);
+  });
+};
+
+registerEvents($);
